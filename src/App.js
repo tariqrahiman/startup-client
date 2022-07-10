a866cda44901f180ce8a4f833b1d0f161b849278
tariq <tariqrahiman@hotmail.com>
2022-07-10 00:19:08 -0700

    Bug Fixes for incorrect routes; added invoice.jsx which was missing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { Outlet, Link } from "react-router-dom";
+import * as React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <h3 class="display-1">QuoteFold</h3>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/invoices">Invoices</Link> |{" "}
+      <h1>Bookkeeper</h1>
+      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+        <Link to="/invoices">Invoices</Link> |{' '}
         <Link to="/expenses">Expenses</Link>
       </nav>
-     <Outlet /> 
-
-      <p class="lead">
-      “Brain is the most powerful device which can work bamboozled in both construction mode and destruction mode” ― J. Nedumaan
-</p>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
+import * as React from 'react';
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import App from "./App";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-import Expenses from "./components/expenses";
-import Invoices from "./components/invoices";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Expenses from './components/expenses';
+import Invoices from './components/invoices';
+import Invoice from './components/invoice';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-        <Route path=":invoiceId" element={<Invoices />} />
+       <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route
-        index
-        element={
-          <main style={{ padding: "1rem" }}>
-            <p>Select an invoice</p>
-          </main>
-        }
-      />
-       <Route path=":invoiceId" element={<Invoices />} />
-      </Route>
-      <Route
-      path="*"
-      element={
-        <main style={{ padding: "1rem" }}>
-          <p>White noise refers to sounds that mask other sounds that might occur naturally in an environment. If you live in a city, for example, white noise could help block out noises associated with traffic. Specific sounds might be used to help encourage sleep regardless of environmental noises.</p>
-        </main>
-      }
-    />
-  {/* </Route> */}
-  </Routes>
-  </BrowserRouter>
-);
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        /> 
+      </Route> 
+    </Routes>
+  </BrowserRouter>,
+ );
+reportWebVitals();
