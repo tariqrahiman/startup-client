4d343bf76d40e31e167e46acad4f4ae1cd9396cf
tariq <tariqrahiman@hotmail.com>
2022-07-09 22:02:24 -0700

    Initial Setup Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 class="display-1">QuoteFold</h3>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+     <Outlet /> 
+
+      <p class="lead">
+      “Brain is the most powerful device which can work bamboozled in both construction mode and destruction mode” ― J. Nedumaan
+</p>
     </div>
   );
 }
-
-export default App;
